2f1c54455412d681cae8dfead302c65bb96dfcf7
Sofía-Méndez-Arroyo <sofia.mendez.external@eviden.com>
2025-05-14 11:36:50 +0200

    Add files via upload

---
+++ src/main/java/com/atos/concesionario/proyecto_concesionario/Config/SeguridadConfig.java
@@ -0,0 +1,15 @@
+package com.atos.concesionario.proyecto_concesionario.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class SeguridadConfig {
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
