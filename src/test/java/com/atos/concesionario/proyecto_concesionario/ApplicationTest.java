24981c68cf52df7dc2da7716ecd7f20f26530de5
camilo1102 <camiloandretti1102@gmail.com>
2025-05-14 12:53:26 +0200

    Test carpetas

--- src/test/java/com/atos/concesionario/proyecto_concesionario/ApplicationTest.java
+++ src/test/java/com/atos/concesionario/proyecto_concesionario/ApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProyectoConcesionarioApplicationTests {
+class ApplicationTest {
 
 	@Test
 	void contextLoads() {
