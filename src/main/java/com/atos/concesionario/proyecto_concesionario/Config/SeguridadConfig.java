ceee2f3513b74191df8da00faf31a412c4bdb422
camilo1102 <camiloandretti1102@gmail.com>
2025-05-20 10:13:51 +0200

    Desabilitado security

--- src/main/java/com/atos/concesionario/proyecto_concesionario/Config/SeguridadConfig.java
+++ src/main/java/com/atos/concesionario/proyecto_concesionario/Config/SeguridadConfig.java
@@ -2,14 +2,21 @@ package com.atos.concesionario.proyecto_concesionario.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class SeguridadConfig {
-	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 }
